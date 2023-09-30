a9beac6a5faeec833f2bc42975dcd59c7019cf30
Branislav Davidovic <davidovicbranislav91@gmail.com>
2023-09-30 19:18:06 +0200

    Added option for creating third-party project

--- src/Infrastructure/Microservice.Template.DependencyInjection/AppServicesExtension.cs
+++ src/Infrastructure/Microservice.Template.DependencyInjection/AppServicesExtension.cs
@@ -1,5 +1,10 @@
-﻿using Microservice.Template.DependencyInjection.Communication;
+﻿
+@*#if(EnableREST || EnableGrpc || EnableMessaging)
+using Microservice.Template.DependencyInjection.Communication;
+#endif*@
+@*#if(EnableThirdParty)
 using Microservice.Template.DependencyInjection.ThirdParty;
+#endif*@
 using Microservice.Template.DependencyInjection.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -12,8 +17,9 @@ namespace Microservice.Template.DependencyInjection
         public static void AddAppServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddPersistance();
+        @*#if(EnableThirdParty)
             services.AddAmadeusServices();
-
+         #endif*@
         @*#if(EnableMessaging)
             services.AddRabbitMq();
         #endif*@
